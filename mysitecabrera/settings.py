485ff7e77102ce43e73a99c51e83d098e661ea14
Demi <cabreradianag8@gmail.com>
2026-06-17 17:52:29 -0600

    actualizaciondemodelos

--- mysitecabrera/settings.py
+++ mysitecabrera/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'mysitecabrera.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysitecabrera',
+        'USER': 'postgres',
+        'PASSWORD': 'diana1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
